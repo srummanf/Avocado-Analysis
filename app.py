0d3c9d6a61fad9e8c00b8cfa549ed5a540af4484
Rumman <rummanfardeen4567@gmail.com>
2023-05-07 19:19:10 +0530

    Files Added

--- app.py
+++ app.py
@@ -1,58 +1,34 @@
-import pandas as pd
-from dash import Dash, dcc, html
+
 
 # The preprocessed data is saved in a CSV and passed as a param in data which is attributw of Dash interface
-data = (
-    pd.read_csv("avocado.csv")
-    .query("type == 'conventional' and region == 'Albany'")
-    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
-    .sort_values(by="Date")
-)
 
-app = Dash(__name__)
 # This section of code is responsible for converting the attributes to HTML tags. Eg: html.H1 ==> <h1></h1> 
 # className is alias to class attribute. Same goes for style
 
 #dcc.Graph is responsible for making the graph. It takes a dictionary as an argument. The dictionary has two keys: data and layout. Data is in the form of CSV and layout we have used for the project is lines
-app.layout = html.Div(
-    children=[
-        html.H1(children="Avocado Analytics",
-                className="header-title",
-                style={"fontSize": "48px", "color": "red"}),
-        html.P(
-            children=(
-                "Analyze the behavior of avocado prices and the number"
-                " of avocados sold in the US between 2015 and 2018"
-            ),
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["AveragePrice"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Average Price of Avocados"},
-            },
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["Total Volume"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Avocados Sold"},
-            },
-        ),
-    ]
-)
+
+
+
+#Once you start the server, Dash will automatically serve the files located in assets/. 
 
 # We can attach external CSS files to our Dash app. We can do this by passing a list of dictionaries to the external_stylesheets argument of the Dash constructor. Each dictionary in the list should have a href key with the URL of the CSS file and a rel key with the value stylesheet. We can use this to add Google Fonts to our app. We can also use this to add CSS files from our local machine. For example, if we have a file called style.css in the same directory as our app.py file, we can add it to our app like this:
+
+
+# rel:"stylesheet" will look for all stylesheet/ CSS files in the assets folder. For JS file you can use external_scripts.
+# app.py
+# app.py
+
+import pandas as pd
+from dash import Dash, Input, Output, dcc, html
+
+data = (
+    pd.read_csv("avocado.csv")
+    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
+    .sort_values(by="Date")
+)
+regions = data["region"].sort_values().unique()
+avocado_types = data["type"].sort_values().unique()
+
 external_stylesheets = [
     {
         "href": (
@@ -62,8 +38,153 @@ external_stylesheets = [
         "rel": "stylesheet",
     },
 ]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "Avocado Analytics: Understand Your Avocados!"
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children="🥑", className="header-emoji"),
+                html.H1(
+                    children="Avocado Analytics", className="header-title"
+                ),
+                html.P(
+                    children=(
+                        "Analyze the behavior of avocado prices and the number"
+                        " of avocados sold in the US between 2015 and 2018"
+                    ),
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Region", className="menu-title"),
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {"label": region, "value": region}
+                                for region in regions
+                            ],
+                            value="Albany",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Type", className="menu-title"),
+                        dcc.Dropdown(
+                            id="type-filter",
+                            options=[
+                                {
+                                    "label": avocado_type.title(),
+                                    "value": avocado_type,
+                                }
+                                for avocado_type in avocado_types
+                            ],
+                            value="organic",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range", className="menu-title"
+                        ),
+                        dcc.DatePickerRange(
+                            id="date-range",
+                            min_date_allowed=data["Date"].min().date(),
+                            max_date_allowed=data["Date"].max().date(),
+                            start_date=data["Date"].min().date(),
+                            end_date=data["Date"].max().date(),
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart",
+                        config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="volume-chart",
+                        config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
+@app.callback(
+    Output("price-chart", "figure"),
+    Output("volume-chart", "figure"),
+    Input("region-filter", "value"),
+    Input("type-filter", "value"),
+    Input("date-range", "start_date"),
+    Input("date-range", "end_date"),
+)
+def update_charts(region, avocado_type, start_date, end_date):
+    filtered_data = data.query(
+        "region == @region and type == @avocado_type"
+        " and Date >= @start_date and Date <= @end_date"
+    )
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["AveragePrice"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Average Price of Avocados",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Total Volume"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {"text": "Avocados Sold", "x": 0.05, "xanchor": "left"},
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    }
+    return price_chart_figure, volume_chart_figure
 
 if __name__ == "__main__":
     app.run_server(debug=True)
+    
