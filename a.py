892da2130baf82386694fbbd8ab5f2ed2c57dc22
Rumman <rummanfardeen4567@gmail.com>
2023-05-10 13:18:47 +0530

    Snippet added

---
+++ a.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from dash import Dash, dcc, html
+
+data = (
+    pd.read_csv("avocado.csv")
+    .query("type == 'conventional' and region == 'Albany'")
+    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
+    .sort_values(by="Date")
+)
+
+app = Dash(__name__)
